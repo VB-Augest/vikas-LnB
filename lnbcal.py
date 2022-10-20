a1f55d3e5d55f324d0d78488ff2c2c87bbd282f4
VB-Augest <116144837+VB-Augest@users.noreply.github.com>
2022-10-20 11:20:14 +0530

    Create lnbcal.py

---
+++ lnbcal.py
@@ -0,0 +1,8 @@
+a=int(input("Enter a number:"))
+b=int(input("Enter 2nd number:"))
+print(a,b)
+print("Product=",a*b)
+if(a*b>500):
+    print("Sum:",a+b)
+else:    
+    print("Hello LNB code is running fine !!")
