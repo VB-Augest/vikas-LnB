f18273ce2ed018979d6b84acbe189a3440ffd8f1
VB-Augest <116144837+VB-Augest@users.noreply.github.com>
2022-10-21 14:04:16 +0530

    Create lnbstrindex.py

---
+++ lnbstrindex.py
@@ -0,0 +1,12 @@
+x= input("enter the string")
+print(x)
+if len(x)>7:
+    i=0
+    for m in x:
+        print(x[i])
+        i =i+2
+if len(x)<7:
+    j=1
+    for n in x:
+        print(x[j])
+        j=j+2
