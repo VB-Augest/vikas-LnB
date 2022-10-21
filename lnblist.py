db9d2d2d1bfe7f3cea41912e2ac2d3cea7ddb980
VB-Augest <116144837+VB-Augest@users.noreply.github.com>
2022-10-21 15:31:18 +0530

    Create lnblist.py

---
+++ lnblist.py
@@ -0,0 +1,12 @@
+L1 = [11, 21, 24, 12, 18]
+L2 = [14, 44, 25, 37, 13]
+L3=[]
+a=0
+while(a<5):
+        L3.append(L1[a])
+        a=a+2
+b=1
+while(b<5):
+        L3.append(L2[b])
+        b=b+2
+print(L3)
